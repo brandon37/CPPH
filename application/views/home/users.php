e5032f6dc0ef2d04c33ad7a6fcf47327ae50e6fb
Develop <develop@Develops-MacBook-Pro.local>
2016-02-29 13:59:00 -0600

    view Users and logout included V2

---
+++ application/views/home/users.php
@@ -0,0 +1,69 @@
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                         Control De Usuarios
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-table"></i> Usuarios
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+                <div class="row">
+                    <div class="col-lg-6">
+                        <h2>Usuarios</h2>
+                        <div class="table-responsive">
+                            <table class="table table-hover table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Email</th>
+                                        <th>Edit</th>
+                                        <th class="text-center">Delete</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                   
+                                    <?php 
+                                        if ($users){
+                                        foreach ($users->result() as $opc) { ?>
+                                            <tr>
+                                                <td><?= $opc->nombre?></td>
+                                                <td><?= $opc->email?></td>
+                                                <td><a href="#">Edit</a></td>
+                                                <td class="text-center text-danger"><a href="#">X</a></td>
+                                            </tr>
+
+
+                                        <?php } 
+                                        }else{
+                                            echo "Error en la aplicacion ";
+                                        }
+                                    ?>           
+                                   
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
